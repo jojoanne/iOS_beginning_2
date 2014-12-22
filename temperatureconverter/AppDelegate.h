f70a5904bb3877db7df07557679f25663105bc0f
joanne <koongjoanne@gmail.com>
2014-12-22 15:25:52 -0800

    Initial Commit

---
+++ temperatureconverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  temperatureconverter
+//
+//  Created by Joanne Koong on 12/22/14.
+//  Copyright (c) 2014 jojoanne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
